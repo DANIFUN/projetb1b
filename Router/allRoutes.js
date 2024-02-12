6c935e0c1fe59662815a4f9d8366e820cd089467
AnhDong3 <deph92@gmail.com>
2024-02-12 10:50:26 +0100

    Ajout du fichier index.html

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/Galerie", "Galerie", "/pages/galerie.html","/js/galerie.js")];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Garage Vincent Parrot";
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@ Ce site est un site vitrine pour le garage Vincent Parrot
 
 #Installation
 
-#Déploiement 1er comm
+#Déploiement 1er commit le 12/2/2024
