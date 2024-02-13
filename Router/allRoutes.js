eea8c615b3407469f324815e6dc0cd1ac9ef312a
AnhDong3 <deph925@gmail.com>
2024-02-13 23:10:10 +0100

    Commit avec tentative de mise en place d'une GRID

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,9 +1,9 @@
 import Route from "./Route.js";
 
-//Définir ici vos routes
+//Définir ici les routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "/pages/galerie.html","/js/galerie.js")];
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Garage Vincent Parrot";
